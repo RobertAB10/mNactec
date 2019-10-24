0d778e4a5097ab8a7cc26dd73a3f5f04bf25c9f0
RobertAB10 <45283806+RobertAB10@users.noreply.github.com>
2019-10-24 13:13:44 +0200

    Add files via upload

---
+++ AplicacionEscritorio/AplicacionEscritorio/NuevaPregunta.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace AplicacionEscritorio
+{
+    partial class NuevaPregunta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNuevaPregunta = new System.Windows.Forms.Label();
+            this.labelPregunta = new System.Windows.Forms.Label();
+            this.textBoxPregunta = new System.Windows.Forms.TextBox();
+            this.textBoxRespuesta1 = new System.Windows.Forms.TextBox();
+            this.textBoxRespuesta2 = new System.Windows.Forms.TextBox();
+            this.textBoxRespuesta3 = new System.Windows.Forms.TextBox();
+            this.textBoxRespuesta4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButtonRespuesta1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonRespuesta2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonRespuesta3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonRespuesta4 = new System.Windows.Forms.RadioButton();
+            this.comboBoxNivel = new System.Windows.Forms.ComboBox();
+            this.labelNivel = new System.Windows.Forms.Label();
+            this.groupBoxRespuestas = new System.Windows.Forms.GroupBox();
+            this.radioButtonRespuestaCorrecta = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBoxRespuestas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelNuevaPregunta
+            // 
+            this.labelNuevaPregunta.AutoSize = true;
+            this.labelNuevaPregunta.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNuevaPregunta.Location = new System.Drawing.Point(260, 44);
+            this.labelNuevaPregunta.Name = "labelNuevaPregunta";
+            this.labelNuevaPregunta.Size = new System.Drawing.Size(187, 22);
+            this.labelNuevaPregunta.TabIndex = 0;
+            this.labelNuevaPregunta.Text = "NUEVA PREGUNTA ";
+            this.labelNuevaPregunta.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelPregunta
+            // 
+            this.labelPregunta.AutoSize = true;
+            this.labelPregunta.Location = new System.Drawing.Point(201, 107);
+            this.labelPregunta.Name = "labelPregunta";
+            this.labelPregunta.Size = new System.Drawing.Size(123, 13);
+            this.labelPregunta.TabIndex = 1;
+            this.labelPregunta.Text = "Escribe aquí la pregunta";
+            // 
+            // textBoxPregunta
+            // 
+            this.textBoxPregunta.Location = new System.Drawing.Point(204, 138);
+            this.textBoxPregunta.Name = "textBoxPregunta";
+            this.textBoxPregunta.Size = new System.Drawing.Size(403, 20);
+            this.textBoxPregunta.TabIndex = 2;
+            // 
+            // textBoxRespuesta1
+            // 
+            this.textBoxRespuesta1.Location = new System.Drawing.Point(72, 56);
+            this.textBoxRespuesta1.Name = "textBoxRespuesta1";
+            this.textBoxRespuesta1.Size = new System.Drawing.Size(219, 20);
+            this.textBoxRespuesta1.TabIndex = 4;
+            // 
+            // textBoxRespuesta2
+            // 
+            this.textBoxRespuesta2.Location = new System.Drawing.Point(72, 119);
+            this.textBoxRespuesta2.Name = "textBoxRespuesta2";
+            this.textBoxRespuesta2.Size = new System.Drawing.Size(219, 20);
+            this.textBoxRespuesta2.TabIndex = 5;
+            // 
+            // textBoxRespuesta3
+            // 
+            this.textBoxRespuesta3.Location = new System.Drawing.Point(393, 56);
+            this.textBoxRespuesta3.Name = "textBoxRespuesta3";
+            this.textBoxRespuesta3.Size = new System.Drawing.Size(219, 20);
+            this.textBoxRespuesta3.TabIndex = 6;
+            // 
+            // textBoxRespuesta4
+            // 
+            this.textBoxRespuesta4.Location = new System.Drawing.Point(393, 119);
+            this.textBoxRespuesta4.Name = "textBoxRespuesta4";
+            this.textBoxRespuesta4.Size = new System.Drawing.Size(219, 20);
+            this.textBoxRespuesta4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "1.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "2.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(371, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "3.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(371, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "4.";
+            // 
+            // radioButtonRespuesta1
+            // 
+            this.radioButtonRespuesta1.AutoSize = true;
+            this.radioButtonRespuesta1.Location = new System.Drawing.Point(296, 59);
+            this.radioButtonRespuesta1.Name = "radioButtonRespuesta1";
+            this.radioButtonRespuesta1.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonRespuesta1.TabIndex = 13;
+            this.radioButtonRespuesta1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRespuesta2
+            // 
+            this.radioButtonRespuesta2.AutoSize = true;
+            this.radioButtonRespuesta2.Location = new System.Drawing.Point(297, 125);
+            this.radioButtonRespuesta2.Name = "radioButtonRespuesta2";
+            this.radioButtonRespuesta2.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonRespuesta2.TabIndex = 14;
+            this.radioButtonRespuesta2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRespuesta3
+            // 
+            this.radioButtonRespuesta3.AutoSize = true;
+            this.radioButtonRespuesta3.Location = new System.Drawing.Point(618, 59);
+            this.radioButtonRespuesta3.Name = "radioButtonRespuesta3";
+            this.radioButtonRespuesta3.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonRespuesta3.TabIndex = 15;
+            this.radioButtonRespuesta3.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRespuesta4
+            // 
+            this.radioButtonRespuesta4.AutoSize = true;
+            this.radioButtonRespuesta4.Location = new System.Drawing.Point(618, 125);
+            this.radioButtonRespuesta4.Name = "radioButtonRespuesta4";
+            this.radioButtonRespuesta4.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonRespuesta4.TabIndex = 16;
+            this.radioButtonRespuesta4.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxNivel
+            // 
+            this.comboBoxNivel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxNivel.FormattingEnabled = true;
+            this.comboBoxNivel.Items.AddRange(new object[] {
+            "Fácil",
+            "Normal",
+            "Difícil"});
+            this.comboBoxNivel.Location = new System.Drawing.Point(571, 48);
+            this.comboBoxNivel.Name = "comboBoxNivel";
+            this.comboBoxNivel.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxNivel.TabIndex = 17;
+            // 
+            // labelNivel
+            // 
+            this.labelNivel.AutoSize = true;
+            this.labelNivel.Location = new System.Drawing.Point(534, 51);
+            this.labelNivel.Name = "labelNivel";
+            this.labelNivel.Size = new System.Drawing.Size(31, 13);
+            this.labelNivel.TabIndex = 18;
+            this.labelNivel.Text = "Nivel";
+            // 
+            // groupBoxRespuestas
+            // 
+            this.groupBoxRespuestas.Controls.Add(this.radioButtonRespuestaCorrecta);
+            this.groupBoxRespuestas.Controls.Add(this.textBoxRespuesta3);
+            this.groupBoxRespuestas.Controls.Add(this.textBoxRespuesta1);
+            this.groupBoxRespuestas.Controls.Add(this.textBoxRespuesta2);
+            this.groupBoxRespuestas.Controls.Add(this.radioButtonRespuesta4);
+            this.groupBoxRespuestas.Controls.Add(this.textBoxRespuesta4);
+            this.groupBoxRespuestas.Controls.Add(this.radioButtonRespuesta3);
+            this.groupBoxRespuestas.Controls.Add(this.label1);
+            this.groupBoxRespuestas.Controls.Add(this.radioButtonRespuesta2);
+            this.groupBoxRespuestas.Controls.Add(this.label2);
+            this.groupBoxRespuestas.Controls.Add(this.radioButtonRespuesta1);
+            this.groupBoxRespuestas.Controls.Add(this.label3);
+            this.groupBoxRespuestas.Controls.Add(this.label4);
+            this.groupBoxRespuestas.Location = new System.Drawing.Point(12, 185);
+            this.groupBoxRespuestas.Name = "groupBoxRespuestas";
+            this.groupBoxRespuestas.Size = new System.Drawing.Size(680, 159);
+            this.groupBoxRespuestas.TabIndex = 19;
+            this.groupBoxRespuestas.TabStop = false;
+            this.groupBoxRespuestas.Text = "Respuestas";
+            // 
+            // radioButtonRespuestaCorrecta
+            // 
+            this.radioButtonRespuestaCorrecta.AutoSize = true;
+            this.radioButtonRespuestaCorrecta.Checked = true;
+            this.radioButtonRespuestaCorrecta.Enabled = false;
+            this.radioButtonRespuestaCorrecta.Location = new System.Drawing.Point(72, 20);
+            this.radioButtonRespuestaCorrecta.Name = "radioButtonRespuestaCorrecta";
+            this.radioButtonRespuestaCorrecta.Size = new System.Drawing.Size(209, 17);
+            this.radioButtonRespuestaCorrecta.TabIndex = 17;
+            this.radioButtonRespuestaCorrecta.TabStop = true;
+            this.radioButtonRespuestaCorrecta.Text = "Marca la respuesta que sea la correcta";
+            this.radioButtonRespuestaCorrecta.UseVisualStyleBackColor = true;
+            this.radioButtonRespuestaCorrecta.CheckedChanged += new System.EventHandler(this.radioButtonRespuestaCorrecta_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(468, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 33);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(595, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 33);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // NuevaPregunta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 431);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBoxRespuestas);
+            this.Controls.Add(this.labelNivel);
+            this.Controls.Add(this.comboBoxNivel);
+            this.Controls.Add(this.textBoxPregunta);
+            this.Controls.Add(this.labelPregunta);
+            this.Controls.Add(this.labelNuevaPregunta);
+            this.Name = "NuevaPregunta";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.NuevaPregunta_Load);
+            this.groupBoxRespuestas.ResumeLayout(false);
+            this.groupBoxRespuestas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNuevaPregunta;
+        private System.Windows.Forms.Label labelPregunta;
+        private System.Windows.Forms.TextBox textBoxPregunta;
+        private System.Windows.Forms.TextBox textBoxRespuesta1;
+        private System.Windows.Forms.TextBox textBoxRespuesta2;
+        private System.Windows.Forms.TextBox textBoxRespuesta3;
+        private System.Windows.Forms.TextBox textBoxRespuesta4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButtonRespuesta1;
+        private System.Windows.Forms.RadioButton radioButtonRespuesta2;
+        private System.Windows.Forms.RadioButton radioButtonRespuesta3;
+        private System.Windows.Forms.RadioButton radioButtonRespuesta4;
+        private System.Windows.Forms.ComboBox comboBoxNivel;
+        private System.Windows.Forms.Label labelNivel;
+        private System.Windows.Forms.GroupBox groupBoxRespuestas;
+        private System.Windows.Forms.RadioButton radioButtonRespuestaCorrecta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
